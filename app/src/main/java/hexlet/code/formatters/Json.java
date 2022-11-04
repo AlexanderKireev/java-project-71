ef1a2056e9f1c5ea2e4cc1996dbe05bc78635b6f
Alexander Kireev <alexanderkir1980@gmail.com>
2022-11-04 11:31:34 +0300

    update formatters

--- app/src/main/java/hexlet/code/formatters/Json.java
+++ app/src/main/java/hexlet/code/formatters/Json.java
@@ -6,32 +6,54 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-
 import static hexlet.code.Differ.getSign;
 //import java.io.File;
 //import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 //import com.fasterxml.jackson.databind.ObjectWriter;
 
 public class Json implements Format {
-    public final String getString(Map<String, Object> dataFile1, Map<String, Object> dataFile2/*,
-                            Map<String, Object> dataSign*/) throws Exception {
+    public final String getString(Map<String, Object> dataFile1, Map<String, Object> dataFile2) throws Exception {
         Map<String, String> dataSign = getSign(dataFile1, dataFile2);
         List<Object> allData = new ArrayList<>();
         for (Map.Entry<String, String> data : dataSign.entrySet()) {
             Map<String, Object> m = new TreeMap<>();
             m.put("operation", data.getValue());
             m.put("name", data.getKey());
-            if (data.getValue().equals("added")
-                    || data.getValue().equals("unchanged")) {
-                m.put("value", dataFile2.get(data.getKey()));
+            switch (data.getValue()) {
+                case "added":
+                    m.put("value", dataFile2.get(data.getKey()));
+                    break;
+                case "unchanged":
+                    m.put("value", dataFile2.get(data.getKey()));
+                    break;
+                case "deleted":
+                    m.put("value", dataFile1.get(data.getKey()));
+                    break;
+                case "changed":
+                    m.put("value1", dataFile1.get(data.getKey()));
+                    m.put("value2", dataFile2.get(data.getKey()));
+                    break;
+                default:
+                    System.out.print("Error value");
             }
-            if (data.getValue().equals("deleted")) {
-                m.put("value", dataFile1.get(data.getKey()));
-            }
-            if (data.getValue().equals("changed")) {
-                m.put("value1", dataFile1.get(data.getKey()));
-                m.put("value2", dataFile2.get(data.getKey()));
-            }
+
+
+
+
+
+
+
+//            if (data.getValue().equals("added")
+//                    || data.getValue().equals("unchanged")) {
+//                m.put("value", dataFile2.get(data.getKey()));
+//            }
+//            if (data.getValue().equals("deleted")) {
+//                m.put("value", dataFile1.get(data.getKey()));
+//            }
+//            if (data.getValue().equals("changed")) {
+//                m.put("value1", dataFile1.get(data.getKey()));
+//                m.put("value2", dataFile2.get(data.getKey()));
+//            }
             allData.add(m);
         }
 //        запись результата в json-файл
--- app/src/main/java/hexlet/code/formatters/Plain.java
+++ app/src/main/java/hexlet/code/formatters/Plain.java
@@ -3,9 +3,7 @@ package hexlet.code.formatters;
 import hexlet.code.Format;
 import org.apache.commons.lang3.ClassUtils;
 import org.apache.commons.lang3.StringUtils;
-
 import java.util.Map;
-
 import static hexlet.code.Differ.getSign;
 
 public class Plain implements Format {
@@ -25,8 +23,7 @@ public class Plain implements Format {
                     break;
                 case "added":
                     result.append("Property '").append(data.getKey())
-                            .append("' was added with value: ")
-                            .append(getValByType(secondVal)).append("\n");
+                            .append("' was added with value: ").append(getValByType(secondVal)).append("\n");
                     break;
                 case "deleted":
                     result.append("Property '").append(data.getKey()).append("' was removed\n");
@@ -34,22 +31,6 @@ public class Plain implements Format {
                 default:
                     System.out.print("Error value");
             }
-
-
-
-//            if (data.getValue().equals("changed")) {
-//                result.append("Property '").append(data.getKey())
-//                        .append("' was updated. From ").append(getValByType(firstVal))
-//                        .append(" to ").append(getValByType(secondVal)).append("\n");
-//            }
-//            if (data.getValue().equals("added")) {
-//                result.append("Property '").append(data.getKey())
-//                        .append("' was added with value: ")
-//                        .append(getValByType(secondVal)).append("\n");
-//            }
-//            if (data.getValue().equals("deleted")) {
-//                result.append("Property '").append(data.getKey()).append("' was removed\n");
-//            }
         }
         return StringUtils.removeEnd(result.toString(), "\n");
     }
