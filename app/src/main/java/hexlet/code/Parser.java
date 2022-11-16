31ea2b27b25516a1add1471a798c5b6412286b5f
AlexanderKireev <102905257+AlexanderKireev@users.noreply.github.com>
2022-11-16 14:34:23 +0300

    Update Parser.java

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -32,8 +32,11 @@ public class Parser {
 
     private static String getFileExtension(File file) {
         String fileName = file.getName();
+        // если в имени файла есть точка и она не является первым символом в названии файла
         if (fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0) {
+            // то вырезаем все знаки после последней точки в названии файла, то есть ХХХХХ.txt -> txt
             return fileName.substring(fileName.lastIndexOf(".") + 1);
+        // в противном случае - расширение не найдено
         } else {
             return "";
         }
