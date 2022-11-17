95eb4367102459335af51de8a82d31fea0ac7e16
Alexander Kireev <alexanderkir1980@gmail.com>
2022-11-17 12:57:13 +0300

    update Parser.java

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -3,17 +3,15 @@ package hexlet.code;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
-import java.io.File;
 import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.nio.file.Path;
 import java.util.Map;
 import java.util.Objects;
 
 public class Parser {
     public static Map<String, Object> getData(String filePath) throws Exception {
         byte[] mapData = readFile(filePath);
-        String fileType = getFileExtension(new File(filePath));
-
+        String fileType = getFileExtension(filePath);
         ObjectMapper mapper;
         if (Objects.equals(fileType, "json")) {
             mapper = new ObjectMapper();
@@ -27,15 +25,15 @@ public class Parser {
     }
 
     public static byte[] readFile(String filePath) throws Exception {
-        return Files.readAllBytes(Paths.get(filePath));
+        Path path = Path.of(filePath).normalize().toAbsolutePath();
+        return Files.readAllBytes(path);
     }
 
-    private static String getFileExtension(File file) {
-        String fileName = file.getName();
+    private static String getFileExtension(String filePath) {
         // если в имени файла есть точка и она не является первым символом в названии файла
-        if (fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0) {
+        if (filePath.lastIndexOf(".") != -1 && filePath.lastIndexOf(".") != 0) {
             // то вырезаем все знаки после последней точки в названии файла, то есть ХХХХХ.txt -> txt
-            return fileName.substring(fileName.lastIndexOf(".") + 1);
+            return filePath.substring(filePath.lastIndexOf(".") + 1);
         // в противном случае - расширение не найдено
         } else {
             return "";
